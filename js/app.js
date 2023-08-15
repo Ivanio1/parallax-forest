760cead45426f23694a98e1811391ef247eafb04
Sobolev Ivan <sobolev1van@yandex.ru>
2023-08-15 12:56:34 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ document.addEventListener('mousemove', e => {
 		`
 	})
 })
+document.addEventListener('touchmove', e => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
+		`
+	})
+})
